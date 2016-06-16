50e9ecd56e22bd66dffcea008d1e1f29e73fe74b
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2016-06-16 23:23:01 +0530

    create

---
+++ NodeDemo/NodeJsApplication/app.js
@@ -0,0 +1,19 @@
+var fs = require('fs')
+
+fs.readFile('package.json', function(err, buf){
+    console.log(buf.toString());
+});
+
+/*
+  
+ var http = require('http');
+
+var server = http.createServer(function (request, response){
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("DONE IT AGAIN!!");
+});
+
+server.listen(7020);
+
+*/
+
